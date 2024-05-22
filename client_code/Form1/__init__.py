e88ef61b490982c5bd9db30246b005aaba5600d3
kadbam@gmail.com <kadbam@gmail.com>
2024-05-22 15:55:36 +0000

    Edited settings, ServerModule1 and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,7 +1,10 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 import anvil.server
-
+import anvil.http
 
 class Form1(Form1Template):
   def __init__(self, **properties):
@@ -9,3 +12,11 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+        # Get the random verse from the server
+        verse_data = anvil.server.call('get_random_verse')
+        
+        # Set the labels with the verse and reference
+        self.verse_label.text = verse_data['verse']
+        self.reference_label.text = verse_data['reference']
+
--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -1,5 +1,11 @@
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.files
+from anvil.files import data_files
 import anvil.server
-
+import pandas as pd
+import random
 # This is a server module. It runs on the Anvil server,
 # rather than in the user's browser.
 #
@@ -12,3 +18,19 @@ import anvil.server
 #   print("Hello, " + name + "!")
 #   return 42
 #
+
+# Load the CSV file
+@anvil.server.callable
+def get_random_verse():
+    df = pd.read_csv(anvil.server.get_app_origin() + "/_/anvil/media/bible_verses.csv")
+    
+    # Use the device's unique ID for a consistent "random" verse
+    device_id = anvil.server.get_current_device()['id']
+    random.seed(device_id)
+    
+    random_verse = df.sample(1).iloc[0]
+    
+    return {
+        'reference': random_verse['reference'],
+        'verse': random_verse['verse']
+    }
